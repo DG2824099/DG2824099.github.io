d59fc91cef414fed27e2a9f921b348993aa7c541
DG2824099 <143962750+DG2824099@users.noreply.github.com>
2025-07-07 01:27:39 -0300

    ,

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ function tocarVideo(caminhoDoVideo) {
     videoPlayer.play();
   }
 }
+const mapContainer = document.querySelector('.map-container');
+
+// Função que será ativada ao clicar no mapa
+function encontrarCoordenadas(event) {
+    // Pega as dimensões do container do mapa
+    const rect = mapContainer.getBoundingClientRect();
+
+    // Calcula a posição do clique dentro do mapa
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    // Calcula a posição em porcentagem
+    const leftPercent = (x / rect.width) * 100;
+    const topPercent = (y / rect.height) * 100;
+
+    // Exibe as coordenadas no console no formato exato que precisamos
+    console.log(`style="top: ${topPercent.toFixed(2)}%; left: ${leftPercent.toFixed(2)}%;"`);
+}
+
+// Adiciona o "escutador" de cliques ao mapa
+mapContainer.addEventListener('click', encontrarCoordenadas);
+
+// --- FIM DO MODO CRIADOR ---
